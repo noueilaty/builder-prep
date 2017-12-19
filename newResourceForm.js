645e54c16bc3d7e2724ebe04ecf9a4ec14a09ac5
Kelvin Moi <kelvin.mai002@gmail.com>
2017-12-19 14:25:34 -0600

    Completed display resources functionality

--- newResourceForm.js
+++ newResourceForm.js
@@ -4,33 +4,38 @@
   let title = $('#resource-title'),
       link = $('#resource-link'),
       topic = $('#resource-topic'),
+      description = $('#resource-description'),
+      display = $('#resource-display'),
       localDB = [];
 
-  db.on('value', snapshot => {
-    localDB = [];
+  db.once('value').then( snapshot => {
     for (let key in snapshot.val()){
       localDB = [...localDB,
         new Resource(
           snapshot.val()[key].title || '',
           snapshot.val()[key].link || '',
           snapshot.val()[key].topic || '',
+          snapshot.val()[key].description || '',
           key,
           snapshot.val()[key].likes || 0
         )];
     }
-    localDB.forEach(resource => resource.updateDB());
+    localDB.forEach(resource => resource.display(display));
     console.log(localDB);
   });
 
   $('#resource-form').on('submit', e => {
     e.preventDefault();
-    if (title.val() && link.val()){
+    if (title.val() && link.val() && description.val()){
       let resource = {
         title: title.val(),
         link: link.val(),
-        topic: topic.val()
+        topic: topic.val(),
+        description: description.val()
       }
       db.push().set(resource);
     }
   });
+
+
 })(window);
