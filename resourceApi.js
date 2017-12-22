a92009266bbe361e2e90c0ef21d063de27a83e40
skyntax9000 <williamtsanders@gmail.com>
2017-12-22 13:34:47 -0600

    styled curriculums

--- resourceApi.js
+++ resourceApi.js
@@ -27,14 +27,38 @@ class Resource {
 
   display(root) {
     this.updateDB();
+    let icon
+    if (this.topic == 'Python') {
+      icon = '<i class="devicon-python-plain colored"></i>'
+    } else if (this.topic == 'JavaScript') {
+      icon = '<i class="devicon-javascript-plain colored"></i>'
+    } else if (this.topic == 'HTML & CSS') {
+      icon = '<i class="fa fa-code fa-1" aria-hidden="true"></i>'
+    } else if (this.topic == 'Other') {
+      icon = '<i class="fa fa-laptop fa-1" aria-hidden="true"></i>'
+
+    }
     root.append(`
       <div class='Resource'>
-        <h3 class='Resource-title'><a href='${this.link}'>${this.title}</a></h3>
-        <p>${this.description}</p>
-        <div class='Resource-info'>
-          <p><strong>Topic:</strong> ${this.topic}</p>
-          <p>${this.likes} likes</p>
+        <div class='Resource-header'>
+          <div class='Resource-icon'>
+            ${icon}
+          </div>
+          <div class='Resource-titleTopicContainer'>
+            <h3 class='Resource-title'><a href='${this.link}'>${this.title}</a></h3>
+            <div class='Resource-topic'>
+              <p> ${this.topic}</p>
+            </div>
+          </div>
         </div>
+        <div class='Resource-description'>
+          <p>${this.description}</p>
+        </div>
+        <div class='Resource-info'>
+        </div>
+          <div class='Resource-likes'>
+            <p>${this.likes} likes</p>
+          </div>
       </div>
     `)
   }
