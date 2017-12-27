98eec5ebbb91920f4b57a89bb2d677ebd2f2acc1
Nydia Segura <nydia.segura@gmail.com>
2017-12-26 22:04:37 -0600

    added the login/username to the list in the slideout menu

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     $('.Auth-off').hide();
     $('.Auth-on').show();
-    $('#username').text(user.email);
+    $('.username').text(user.email);
   } else {
     $('.Auth-off').show();
     $('.Auth-on').hide();
