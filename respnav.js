8a60095bb56306bd8557e591c2eb7907799cba31
Nydia Segura <nydia.segura@gmail.com>
2017-12-26 21:32:49 -0600

    added hamburger nav bar functionality

---
+++ respnav.js
@@ -0,0 +1,8 @@
+function openSideMenu(){
+    document.getElementById('side-menu').style.width = '250px';
+}
+
+
+function closeSideMenu(){
+    document.getElementById('side-menu').style.width = '0';
+}
